a767e2d8d4b31a33ae8270d60401c3b2be0ee252
Eric Elias <ericelias36@gmail.com>
2022-04-21 14:50:16 -0400

    added high level interface functs

--- src/hardware_interface/src/hardware_interface.py
+++ src/hardware_interface/src/hardware_interface.py
@@ -1,18 +1,43 @@
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, ByteMultiArray, Float32
+from sensor_msgs.msg import Imu
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+
+
+class Hardware_interface:
+    def __init__(self):
+        rospy.init_node('hardware_interface_node', anonymous=True, disable_signals=True)
+
+        rospy.Subscriber("/relay_states", ByteMultiArray, self.relay_states_callback, queue_size=1)
+        rospy.Subscriber("/driver_speed", Float32, self.driver_speed_callback, queue_size=1)
+
+        self.imu_pub = rospy.Publisher("/imu/data_raw", Imu, queue_size=1)
+
+
+
+    def relay_states_callback(self, data):
+        return
+
+    def driver_speed_callback(self, data):
+        return
+        
+    def start_running(self):
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            self.imu_pub.publish( self.imu_msg() )
+            rate.sleep()
+
+
+# def talker():
+#     pub = rospy.Publisher('chatter', String, queue_size=10)
+#     rospy.init_node('talker', anonymous=True)
+#     rate = rospy.Rate(10) # 10hz
+#     while not rospy.is_shutdown():
+#         hello_str = "hello world %s" % rospy.get_time()
+#         rospy.loginfo(hello_str)
+#         pub.publish(hello_str)
+#         rate.sleep()
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+    hardware_interface = Hardware_interface()
+    hardware_interface.start_running()
