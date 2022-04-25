415c95345768670867b1c23fb7257e56c59036ac
Eric Elias <ericelias36@gmail.com>
2022-04-24 22:00:14 -0400

    got py module to import

--- src/hardware_interface/setup.py
+++ src/hardware_interface/setup.py
@@ -1,11 +1,8 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-
-from setuptools import setup
+from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['imu'],
-    package_dir={'': 'src'})
-
-setup(**setup_args)
+d = generate_distutils_setup(
+    packages=['hardware_interface'],
+    package_dir={'': 'src'}
+)
+setup(**d)
--- src/hardware_interface/src/imu/__init__.py
+++
@@ -1,1 +0,0 @@
-# from . import imu_test
