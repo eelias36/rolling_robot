dcca9718665d330ea1f38d6233c3db86595e6a44
Eric Elias <ericelias36@gmail.com>
2022-04-25 14:50:06 -0400

    imu fully working in software

--- src/state_estimation/src/state_estimation_node.cpp
+++ src/state_estimation/src/state_estimation_node.cpp
@@ -20,6 +20,7 @@ main( int argc, char* argv[] ){
 	ros::Subscriber imu_subscriber = node_handle.subscribe( "/imu/data",1, &State_Estimation::handle_imu_msg, &state_estimation );
 	ros::Subscriber heading_subscriber = node_handle.subscribe( "/cmd_heading",1, &State_Estimation::handle_heading_msg, &state_estimation );
 	ros::Subscriber rolling_subscriber = node_handle.subscribe( "/rolling_state",1, &State_Estimation::handle_rolling_msg, &state_estimation );
+	
 	ros::Publisher mag_publisher = node_handle.advertise< sensor_msgs::MagneticField >( "/imu/mag", 1, true );
 	ros::Publisher pos_publisher = node_handle.advertise< geometry_msgs::Point >( "/position", 1, true );
 	ros::Publisher particle_publisher = node_handle.advertise< sensor_msgs::PointCloud >( "/particles", 1, true );
@@ -27,16 +28,16 @@ main( int argc, char* argv[] ){
 	ros::Publisher bad_pos_error_publisher = node_handle.advertise< std_msgs::Float64 >( "/bad_pos_error", 1, true );	
 	ros::Publisher good_pos_error_publisher = node_handle.advertise< std_msgs::Float64 >( "/good_pos_error", 1, true );	
 
-	ros::Timer filter_timer = node_handle.createTimer(ros::Duration(0.1), &State_Estimation::callback, &state_estimation);
+	// ros::Timer filter_timer = node_handle.createTimer(ros::Duration(0.1), &State_Estimation::callback, &state_estimation);
 
 
 	double frequency = 10;
 	ros::Rate timer( frequency );
 
 	while( ros::ok() ){
-		mag_publisher.publish( state_estimation.mag_field_msg() );
-		pos_publisher.publish( state_estimation.pos_msg() );
-		particle_publisher.publish( state_estimation.particle_msg() );
+		// mag_publisher.publish( state_estimation.mag_field_msg() );
+		// pos_publisher.publish( state_estimation.pos_msg() );
+		// particle_publisher.publish( state_estimation.particle_msg() );
 		pose_publisher.publish( state_estimation.pose_msg() );
 		// bad_pos_error_publisher.publish( state_estimation.bad_estimate_error_msg() );
 		// good_pos_error_publisher.publish( state_estimation.particle_estimate_error_msg() );
