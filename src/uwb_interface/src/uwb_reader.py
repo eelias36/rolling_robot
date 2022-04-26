3e813b25b97d0072f7f89bef8a9de663192e0142
Eric Elias <ericelias36@gmail.com>
2022-04-26 16:41:17 -0400

    got uwb reader working

--- src/uwb_interface/src/uwb_reader.py
+++ src/uwb_interface/src/uwb_reader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import Point
 
 import serial
 import time
@@ -9,7 +9,7 @@ import time
 uwb_reader.py
 Alex Elias
 
-Sends UWB LEC readings to ROS ropic as a string
+Sends UWB LEP readings to ROS ropic as a string
 
 Parameters:
     serial_port: e.g. '/dev/ttyS0'
@@ -23,9 +23,12 @@ class Uwb_reader:
         self.serial_port = rospy.get_param('~serial_port')
         topic_name = rospy.get_param('~topic_name')
 
+        # self.serial_port = '/dev/ttyACM0'
+        # topic_name = 'uwb_1'
+
         self.ser = None
 
-        self.pub = rospy.Publisher(topic_name, String, queue_size=1)
+        self.pub = rospy.Publisher(topic_name, Point, queue_size=1)
         
         rospy.on_shutdown(self.close_serial_if_active)
 
@@ -33,20 +36,20 @@ class Uwb_reader:
         if(not(self.ser == None)):
             self.ser.close()
 
-    def start_lec_mode(self):
+    def start_lep_mode(self):
        # Reset UWB tag so that we're in a known state
-        self.ser.write('reset\r')
-        self.ser.write('reset\r')
+        self.ser.write('reset\r'.encode())
+        self.ser.write('reset\r'.encode())
 
         time.sleep(0.1)
         self.ser.reset_input_buffer()
         self.ser.reset_output_buffer()
         
         ser_bytes = self.ser.readline()
-        while not 'dwm>' in ser_bytes:
+        while not 'dwm>'.encode() in ser_bytes:
             rospy.loginfo('waiting for dwm>')
             rospy.loginfo(ser_bytes)
-            self.ser.write('\r\r')
+            self.ser.write('\r\r'.encode())
             ser_bytes = self.ser.readline()
             time.sleep(0.1)
             while(self.ser.in_waiting):
@@ -55,7 +58,7 @@ class Uwb_reader:
                 time.sleep(0.1)
 
         # Tell UWB tag to give us distance readings
-        self.ser.write("lec\r")
+        self.ser.write("lep\r".encode())
 
         ser_bytes = self.ser.readline() 
 
@@ -72,11 +75,31 @@ class Uwb_reader:
                     # self.ser.reset_input_buffer()
                     # self.ser.reset_output_buffer()
                     time.sleep(1)
-                    self.start_lec_mode()
+                    self.start_lep_mode()
 
                 ser_bytes = self.ser.readline()
                 if(ser_bytes):
-                    self.pub.publish(ser_bytes)
+                    try:
+                        # self.pub.publish(ser_bytes)
+                        # print(ser_bytes.decode())
+
+                        # create list of x,y,z,QF
+                        pos_list_str = ser_bytes.decode().split(",")
+                        pos_list = list()
+                        for i in range(1,5):
+                            pos_list.append( float( pos_list_str[i] ) )
+                        # print( pos_list )
+
+                        # publish msg
+                        msg = Point()
+                        msg.x = pos_list[0]
+                        msg.y = pos_list[1]
+                        msg.z = pos_list[2]
+
+                        self.pub.publish( msg )
+                    except:
+                        rospy.logwarn("Error parsing serial message")
+
                 else:
                     rospy.logwarn("Serial timeout occured")
 
